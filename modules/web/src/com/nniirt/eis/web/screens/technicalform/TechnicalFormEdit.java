7f515507097fe63428d98c9e0b3a974d1e02a599
MOKIN VLADISLAV <vladmk@mail.ru>
2022-08-22 08:18:56 +0300

    Исправления разметки в ТА и заготовка для отчетов FastReport

--- modules/web/src/com/nniirt/eis/web/screens/technicalform/TechnicalFormEdit.java
+++ modules/web/src/com/nniirt/eis/web/screens/technicalform/TechnicalFormEdit.java
@@ -4,19 +4,23 @@ import com.haulmont.cuba.core.app.FileStorageService;
 import com.haulmont.cuba.core.entity.FileDescriptor;
 import com.haulmont.cuba.core.global.DataManager;
 import com.haulmont.cuba.core.global.FileStorageException;
+import com.haulmont.cuba.gui.Dialogs;
 import com.haulmont.cuba.gui.Notifications;
-import com.haulmont.cuba.gui.components.Action;
-import com.haulmont.cuba.gui.components.FileUploadField;
-import com.haulmont.cuba.gui.components.Table;
+import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.export.ExportDisplay;
 import com.haulmont.cuba.gui.export.ExportFormat;
 import com.haulmont.cuba.gui.model.CollectionPropertyContainer;
 import com.haulmont.cuba.gui.screen.*;
 import com.haulmont.cuba.gui.upload.FileUploadingAPI;
+import com.haulmont.reports.gui.actions.EditorPrintFormAction;
+import com.haulmont.reports.gui.actions.list.ListPrintFormAction;
+import com.haulmont.reports.gui.actions.list.RunReportAction;
 import com.nniirt.eis.entity.qs.TechnicalForm;
 import org.slf4j.Logger;
 
 import javax.inject.Inject;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -38,25 +42,22 @@ public class TechnicalFormEdit extends StandardEditor<TechnicalForm> {
 
     @Inject
     protected ExportDisplay exportDisplay;
-
-    @Inject
-    private DataManager dataManager;
-
-    @Inject
-    private FileStorageService fileStorageService;
-
     @Inject
     protected CollectionPropertyContainer<FileDescriptor> filesDc;
-
     @Inject
     protected Logger logger;
-
     @Inject
     protected Table<FileDescriptor> filesTable;
-
     @Inject
     protected MessageBundle messageBundle;
-
+    @Inject
+    protected BrowserFrame browserFrame;
+    @Inject
+    protected Button printBtn;
+    @Inject
+    private DataManager dataManager;
+    @Inject
+    private FileStorageService fileStorageService;
     private List<FileDescriptor> newImageDescriptors = new ArrayList<>();
 
     @Subscribe("filesTable.download")
@@ -67,6 +68,7 @@ public class TechnicalFormEdit extends StandardEditor<TechnicalForm> {
     private void downloadFile(FileDescriptor file) {
         exportDisplay.show(file, ExportFormat.OCTET_STREAM);
     }
+
     @Subscribe("upload")
     protected void onUploadFileUploadSucceed(FileUploadField.FileUploadSucceedEvent event) {
         FileDescriptor imageDescriptor = upload.getFileDescriptor();
@@ -80,15 +82,29 @@ public class TechnicalFormEdit extends StandardEditor<TechnicalForm> {
             filesDc.getMutableItems().add(savedImageDescriptor);
 
             notifications.create(Notifications.NotificationType.TRAY)
-                    .withCaption(messageBundle.getMessage("fileStoredSuccessfully"))
-                    .show();
+                    .withCaption(messageBundle.getMessage("fileStoredSuccessfully")).show();
         } catch (FileStorageException e) {
             String failedMessage = messageBundle.getMessage("fileStorageFailed");
-            notifications.create(Notifications.NotificationType.ERROR)
-                    .withCaption(failedMessage)
-                    .show();
+            notifications.create(Notifications.NotificationType.ERROR).withCaption(failedMessage).show();
 
             logger.error(failedMessage, e);
+        }
+    }
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+        printBtn.setAction(new EditorPrintFormAction(this, null));
+    }
+
+    @Subscribe
+    protected void onAfterShow(AfterShowEvent event) {
+        TechnicalForm thForm = getEditedEntity();
+
+        try {
+            browserFrame.setSource(UrlResource.class).setUrl(
+                    new URL("http://rtvm-test15.nniirt.local:5000/?reportName=act&reportId=" + thForm.getId().toString()));
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
         }
     }
 
